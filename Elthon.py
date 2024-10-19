d33f67a27e537977fe909e98731876d2c247bf18
Blewog <1004392@newcastle.k12.ok.us>
2024-10-18 22:19:44 -0500

    idk man

--- Elthon.py
+++ Elthon.py
@@ -1,15 +1,11 @@
-# Global variables
-global bold
-global underline
-global end
 
-bold = '\033[1m'
-underline = '\033[4m'
-end = '033[0m'
 
 class write:
   def console(text, option):
     if(option != "return"):
-      print(bold + text + end, end=option)
+      print(text, end=option)
     else:
-      print(underline + text + end)
+      print(text)
+  def undrline(text, option):
+    if(option != "return"):
+      print('{:s}'.format('\u0333').join(text), end=option)
