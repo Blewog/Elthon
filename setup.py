f4c3619c57c2cc3ba663728449ef33c1e99973d2
Blewog <1004392@newcastle.k12.ok.us>
2024-10-19 17:22:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-from setuptools import setup, find_packages
-
-setup()
+from setuptools import setup
+setup(name='Elthon',
+version='0.0.1',
+description='Package setup script',
+url='https://www.github.com/Blewog/Elthon',
+author='anonymous',
+author_email='1004392@newcastle.k12.ok.us',
+license='MPL 2.0',
+packages=['elthon'],
+zip_safe=False)
