5ff9629c929a4f070fed9350633a605a9551eaa0
Blewog <1004392@newcastle.k12.ok.us>
2024-10-21 09:58:24 -0500

    Update ModuleTesting.py

--- ModuleTesting.py
+++ ModuleTesting.py
@@ -3,8 +3,5 @@
 import sys
 from Elthon import write, save, message
 
-write.console("Hello")
-response = sys.stdin.readline()
-save.text(response)
-save.history()
-write.console("", history=True)
+response = input("Hi ")
+write.console(response)
